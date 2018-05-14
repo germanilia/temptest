8e88686a9b04262c5b560313a5fee9334367557b
iliagerman <iliagerman@gmail.com>
2018-05-14 09:03:01 +0300

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http: HttpClient) {
+
+    this.getVerticals().subscribe((verticals) => {
+      console.log('verticals: ', verticals);
+    });
+
+  }
+
+  public getVerticals(): Observable<any> {
+
+    return this.http.get('http://rmsiisws:8080/RMS/verticals/');
+  }
 }
